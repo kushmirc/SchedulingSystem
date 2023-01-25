989452b4190d4457a877d1118928c13d59e7e1f8
kushmirc <107213928+kushmirc@users.noreply.github.com>
2023-01-24 21:40:51 -0700

    Update JDBC class.

--- src/main/java/helper/JDBC.java
+++ src/main/java/helper/JDBC.java
@@ -1,4 +1,38 @@
 package helper;
 
-public class JDBC {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class JDBC {
+
+    private static final String protocol = "jdbc";
+    private static final String vendor = ":mysql:";
+    private static final String location = "//localhost/";
+    private static final String databaseName = "client_schedule";
+    private static final String jdbcURL = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER";
+    private static final String driver = "com.mysql.cj.jdbc.driver";
+    private static final String userName = "sqlUser";
+    private static final String password = "Passw0rd!";
+    public static Connection connection;
+
+    public static void  openConnection(){
+        try{
+            Class.forName(driver);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection Successful");
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void closeConnection(){
+        try{
+            connection.close();
+            System.out.println("Connection Closed");
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module mirchandani.schedulingsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens mirchandani.schedulingsystem to javafx.fxml;
