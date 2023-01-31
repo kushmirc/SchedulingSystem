ca37c0a045e1404038ac13a32d98d05904066649
kushmirc <107213928+kushmirc@users.noreply.github.com>
2023-01-31 16:22:25 -0700

    Added Nat_es.properties file.

---
+++ src/main/java/utility/RBMain.java
@@ -0,0 +1,15 @@
+package utility;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class RBMain {
+
+    public static void main(String[] args) {
+
+        ResourceBundle rb = ResourceBundle.getBundle("utility/Nat", Locale.getDefault());
+
+        if(Locale.getDefault().getLanguage().equals("es") || Locale.getDefault().getLanguage().equals("fr"));
+            System.out.println(rb.getString("hello") + " " + rb.getString("world"));
+    }
+}
