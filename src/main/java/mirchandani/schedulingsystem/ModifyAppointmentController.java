949fd4703a2d568ab39c9f2d2658079d62956a6e
kushmirc <107213928+kushmirc@users.noreply.github.com>
2023-02-08 18:17:59 -0700

    Updated ModifyAppointmentController.

--- src/main/java/mirchandani/schedulingsystem/ModifyAppointmentController.java
+++ src/main/java/mirchandani/schedulingsystem/ModifyAppointmentController.java
@@ -158,7 +158,6 @@ public class ModifyAppointmentController implements Initializable {
                     + "WHERE first_level_divisions.Country_ID = countries.Country_ID "
                     + "AND countries.Country = \"" + location1 + "\"";
 
-
             PreparedStatement ps = JDBC.connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             apptLocationCmb2.getItems().clear();
@@ -170,6 +169,82 @@ public class ModifyAppointmentController implements Initializable {
         catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private void initializeCustomerId() {
+        try{
+            String sql = "SELECT Customer_ID FROM customers ORDER BY Customer_ID ASC";
+            PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                apptCustomerIDCmb.getItems().add(rs.getString("Customer_ID"));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void initializeUserId() {
+        try{
+            String sql = "SELECT User_ID FROM users ORDER BY User_ID ASC";
+            PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                apptUserIDCmb.getItems().add(rs.getString("User_ID"));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void initializeContact() {
+        try{
+            String sql = "SELECT Contact_Name FROM contacts ORDER BY Contact_Name ASC";
+            PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                apptContactCmb.getItems().add(rs.getString("Contact_Name"));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void initializeType() {
+        try{
+            String sql = "SELECT DISTINCT Type FROM appointments ORDER BY Type ASC";
+            PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                apptTypeCmb.getItems().add(rs.getString("Type"));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void initializeTimes() {
+        apptStartTimeHHCmb.getItems().addAll(
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
+
+        apptStartTimeMMCmb.getItems().addAll(
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60");
+
+        apptStartTimeSSCmb.getItems().addAll(
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60");
+
+        apptEndTimeHHCmb.getItems().addAll(
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
+
+        apptEndTimeMMCmb.getItems().addAll(
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60");
+
+        apptEndTimeSSCmb.getItems().addAll(
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60");
     }
 
     @Override
@@ -195,6 +270,12 @@ public class ModifyAppointmentController implements Initializable {
         getLocation1FromLocation2();
         initializeLocation1();
         initializeLocation2();
+        initializeCustomerId();
+        initializeUserId();
+        initializeContact();
+        initializeType();
+        initializeTimes();
 
     }
 }
+
