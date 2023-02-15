727a08687f57c0253b5b8475f13491f7f24b8ace
kushmirc <107213928+kushmirc@users.noreply.github.com>
2023-02-15 15:17:33 -0700

    Added javadoc comments to DisplayNotification and TimeConversion interfaces.

--- src/main/java/utility/DisplayNotification.java
+++ src/main/java/utility/DisplayNotification.java
@@ -2,7 +2,11 @@ package utility;
 
 import java.sql.SQLException;
 
+/** Interface DisplayNotification is used to create a display notification method.
+ * It is a functional interface and has one method for displaying a notification.
+ * @author Kush Mirchandani*/
 public interface DisplayNotification {
 
+    /** This method is used to display a confirmation window. */
     void confirmation() throws SQLException;
 }
--- src/main/java/utility/TimeConversion.java
+++ src/main/java/utility/TimeConversion.java
@@ -3,7 +3,12 @@ package utility;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
+/** Interface TimeConversion is used to create a method for converting date and time values.
+ * It is a functional interface and has one method for converting a ZonedDateTime value to a LocalDateTime value.
+ * @author Kush Mirchandani*/
 public interface TimeConversion {
 
+    /** This method is used to convert a ZonedDateTime value to a LocalDateTime value.
+     * @param zonedDateTime the zonedDateTime value to be converted*/
     LocalDateTime zonedToLdtUtc(ZonedDateTime zonedDateTime);
 }
